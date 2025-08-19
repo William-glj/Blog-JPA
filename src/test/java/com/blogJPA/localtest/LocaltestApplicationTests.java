3dd3e0d3eb812aba6cbc19ecc18cb703eecb34e8
William-glj <guillermolemos0024@gmail.com>
2025-08-19 10:31:42 +0200

    sin comentarios

---
+++ src/test/java/com/blogJPA/localtest/LocaltestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogJPA.localtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocaltestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
